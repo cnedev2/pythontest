9c0d6d9ea126151b25dfd70183b795ac59d7d4a3
cnedev2 <w@t.de>
2017-09-13 17:12:07 +0200

    Added output

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,22 @@
+import sys
+import os
+
 from flask import Flask
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!!!!!!!!!!!!!!!!!!1"
+
+    ret = "Hello World!<br>"
+    ret += "<p>sys.path = " + str(sys.path) + "</p>\n"
+    ret += "<p>os.getcwd() = " + str(sys.path) + "</p>\n"
+    ret += "<p>Environment:<br>"
+
+    for name in os.environ:
+        ret += name + " = " + os.environ[name] + "<br>\n"
+
+    return ret
 
 if __name__ == "__main__":
     application.run()
